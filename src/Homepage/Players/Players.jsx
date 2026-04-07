ac9ebb7d0c90dd1991c54954e9253434aebd64fe
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-07 23:19:22 +0600

    commit

--- src/Homepage/Players/Players.jsx
+++ src/Homepage/Players/Players.jsx
@@ -1,14 +1,16 @@
 import React, { use } from 'react';
+import AvailablePlayers from '../../components/AvailablePlayers/AvailablePlayers';
 
 const Players = ({playersPromise}) => {
   console.log(playersPromise);
-  const data = use(playersPromise);
-  console.log(data);
+  const players = use(playersPromise);
+  console.log(players);
   
   
   return (
     <div>
-      players
+      players : {players.length}
+      <AvailablePlayers players={players}></AvailablePlayers>
     </div>
   );
 };
---
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AvailablePlayers = ({ players }) => {
+  console.log(players, 'players');
+
+  return (
+    <div>
+      AvailablePlayers
+    </div>
+  );
+};
+
+export default AvailablePlayers;
