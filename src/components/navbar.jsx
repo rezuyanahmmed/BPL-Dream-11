34207965b69c0c60a3ef931525291f5e7297ee66
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-16 19:23:27 +0600

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,8 @@ import dollarImg from "../assets/dollar 1.png"
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm container mx-auto">
+    <div className='bg-base-100 shadow-sm'>
+      <div className="navbar container mx-auto">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
