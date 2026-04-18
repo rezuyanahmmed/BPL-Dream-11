6aa158b08f54e1a1921b6a8978955da27b6f6036
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-18 23:34:38 +0600

    commit

--- src/Homepage/Players/AvailablePlayers/AvailablePlayers.jsx
+++ src/Homepage/Players/AvailablePlayers/AvailablePlayers.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaUser, FaFlag } from "react-icons/fa";
+import Card from '../../../components/UI/Card';
+
 // import { FaFlag } from "react-icons/fa";
 
 
@@ -12,38 +13,8 @@ const AvailablePlayers = ({ players }) => {
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-6'>
         {players.map((player) => {
-          return <div className="card bg-base-100 shadow-sm border-gray-300">
-            <figure>
-              <img className='rounded-[10px]'
-                src={player.playerImg}
-                alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title"><FaUser />
-                {player.playerName}</h2>
-
-              <div className='flex justify-between gap-2'>
-                <div className='flex gap-2 items-center'>
-                  <FaFlag />
-                  <p>{player.playerCountry}</p>
-                </div>
-
-                <button className='btn'>{player.playerType}</button>
-              </div>
-              <div className="divider"></div>
-              <h2 className='font-bold'>Rating ({player.rating})</h2>
-              <div className='flex justify-between items-center mt-3'>
-                <p className='font-bold'>{player.battingStyle}</p>
-                <p className='text-right'>{player.bowlingStyle}</p>
-              </div>
-              <div className='flex justify-between items-center pt-5'>
-                <p className='font-bold'>Price: {player.price}</p>
-                <button className='btn'>Choose Player</button>
-              </div>
-
-
-            </div>
-          </div>;
+          return <Card player={player}></Card>
+            
         })}
       </div>
 
---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaUser, FaFlag } from "react-icons/fa";
+
+const Card = ({ player }) => {
+  return (
+    <div className="card bg-base-100 shadow-sm border-gray-300">
+      <figure>
+        <img className='rounded-[10px]'
+          src={player.playerImg}
+          alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title"><FaUser />
+          {player.playerName}</h2>
+
+        <div className='flex justify-between gap-2'>
+          <div className='flex gap-2 items-center'>
+            <FaFlag />
+            <p>{player.playerCountry}</p>
+          </div>
+
+          <button className='btn'>{player.playerType}</button>
+        </div>
+        <div className="divider"></div>
+        <h2 className='font-bold'>Rating ({player.rating})</h2>
+        <div className='flex justify-between items-center mt-3'>
+          <p className='font-bold'>{player.battingStyle}</p>
+          <p className='text-right'>{player.bowlingStyle}</p>
+        </div>
+        <div className='flex justify-between items-center pt-5'>
+          <p className='font-bold'>Price: {player.price}</p>
+          <button className='btn'>Choose Player</button>
+        </div>
+
+
+      </div>
+    </div>;
+  );
+};
+
+export default Card;
