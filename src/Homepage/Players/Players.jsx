9e93c69635f379cdf71b70b06c1d6e0afb3dd9f9
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-19 00:57:49 +0600

    commit

--- src/Homepage/Players/Players.jsx
+++ src/Homepage/Players/Players.jsx
@@ -8,7 +8,15 @@ const Players = ({ playersPromise }) => {
 
   return (
     <div className='w-11/12 max-w-[80%] mx-auto'>
-      players : {players.length}
+      <div className='flex  justify-between mt-5 mb-5'>
+        <h2 className='font-bold text-2xl'>Available Players</h2>
+        <div className='join'>
+          <button className="btn bg-[#E7FE29]">Available</button>
+          <button className="btn btn">Selected</button>
+
+        </div>
+      </div>
+
       <AvailablePlayers players={players}></AvailablePlayers>
     </div>
   );
