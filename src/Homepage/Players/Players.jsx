6224047c0044f97c1c6ac376b0d1d56765506c62
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-19 01:23:37 +0600

    commit

--- src/Homepage/Players/Players.jsx
+++ src/Homepage/Players/Players.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
 const Players = ({ playersPromise }) => {
   console.log(playersPromise);
@@ -6,13 +6,21 @@ const Players = ({ playersPromise }) => {
   // console.log(players);
 
 
+  const [selectedType, setSelectedType] = useState('available');
+  console.log(selectedType, 'selectedType');
+
+
+
   return (
     <div className='w-11/12 max-w-[80%] mx-auto'>
       <div className='flex  justify-between mt-5 mb-5'>
         <h2 className='font-bold text-2xl'>Available Players</h2>
-        <div className='join'>
-          <button className="btn bg-[#E7FE29]">Available</button>
-          <button className="btn btn">Selected</button>
+
+        <div className='join '>
+
+          <button className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : "bg-red-500"} `}>Available</button>
+
+          <button className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : "bg-red-500"} `}>Selected</button>
 
         </div>
       </div>
