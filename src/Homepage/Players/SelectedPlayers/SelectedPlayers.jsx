4d8b08352079d6496e3400fb3e842b5602599170
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-20 01:45:13 +0600

    commit

--- src/Homepage/Players/SelectedPlayers/SelectedPlayers.jsx
+++ src/Homepage/Players/SelectedPlayers/SelectedPlayers.jsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
 import { MdDelete } from "react-icons/md";
-import { FaRegUser } from "react-icons/fa";
+// import { FaRegUser } from "react-icons/fa";
 
 
 
 const SelectedPlayers = ({ selectedPlayers }) => {
   console.log(selectedPlayers);
+
+  const handleDeleteSelectedPlayer = (player) => {
+    console.log(player, 'player');
+
+  }
+
   return (
     <div>
-      {
-        selectedPlayers.map((player, ind) => {
+      <div className='space-y-4 mb-2'>
+        {selectedPlayers.map((player, ind) => {
           return (
             <div key={ind} className='flex items-center gap-6 justify-between p-4 rounded-2xl border'>
               <div className='flex items-center gap-6'>
-                <img src={player.playerImg} alt={player.playerName} 
-                className='h-[100px] w-auto rounded-md' />
+                <img src={player.playerImg} alt={player.playerName}
+                  className='h-[75px] w-auto rounded-md' />
                 <div>
-                  <h2 className='flex items-center gap-4 font-bold text-2xl'><FaRegUser />
+                  <h2 className='flex items-center font-bold text-xl'>
                     {player.playerName}</h2>
-                  <p>{player.playerType}</p>
+                  <p className='font-semibold'>{player.playerType}</p>
                 </div>
               </div>
-              <button><MdDelete /></button>
+              <button className='btn text-red-500' 
+              onClick={() => handleDeleteSelectedPlayer(player)}><MdDelete /></button>
 
             </div>
           );
         })}
+      </div>
     </div>
 
   );
